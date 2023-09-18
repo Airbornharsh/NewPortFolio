afe87592bb6aa9e25f6864ee9043af49529d05ab
Airbornharsh <harshkeshri1234567@gmail.com>
2023-09-18 19:52:26 +0530

    Added Title And Description

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Harsh Keshri | Developer</title>
+        <meta name="description" content="I am Developer who is ready to take challenges you can throw any challenge." />
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="harsh keshri, harsh, keshri, harshkeshri, airbornharsh, developer, FullStackDeveloper, Harsh Keshri, HarshKeshri" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
